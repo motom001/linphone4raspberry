446eb12c126b91066349ad64432164859a0602eb
Thomas <motom001@gmail.com>
2015-09-26 17:50:39 +0200

    Echttest 2015-26-09 um 17-50-39

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -3,8 +3,6 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 set -x # Print commands and their arguments as they are executed.
 
-exit 0
-
 # sudo dpkg --add-architecture i386
 # sudo echo "foreign-architecture i386" > /etc/dpkg/dpkg.cfg.d/multiarch
 # sudo apt-get update -qq
@@ -14,17 +12,16 @@ mkdir ~/raspberrypi
 mkdir ~/raspberrypi/rootfs
 mkdir ~/raspberrypi/tools
 
-echo SET(CMAKE_SYSTEM_NAME Linux) > ~/home/raspberrypi/pi.cmake
-echo SET(CMAKE_SYSTEM_VERSION 1) >> ~/home/raspberrypi/pi.cmake
-echo SET(CMAKE_C_COMPILER $ENV{HOME}/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-gcc) >> ~/home/raspberrypi/pi.cmake
-echo SET(CMAKE_CXX_COMPILER $ENV{HOME}/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-g++) >> ~/home/raspberrypi/pi.cmake
-echo SET(CMAKE_FIND_ROOT_PATH $ENV{HOME}/raspberrypi/rootfs) >> ~/home/raspberrypi/pi.cmake
-echo SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER) >> ~/home/raspberrypi/pi.cmake
-echo SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY) >> ~/home/raspberrypi/pi.cmake
-echo SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY) >> ~/home/raspberrypi/pi.cmake
+echo "SET(CMAKE_SYSTEM_NAME Linux)" > ~/home/raspberrypi/pi.cmake
+echo "SET(CMAKE_SYSTEM_VERSION 1)" >> ~/home/raspberrypi/pi.cmake
+echo "SET(CMAKE_C_COMPILER $ENV{HOME}/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-gcc)" >> ~/home/raspberrypi/pi.cmake
+echo "SET(CMAKE_CXX_COMPILER $ENV{HOME}/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-g++)" >> ~/home/raspberrypi/pi.cmake
+echo "SET(CMAKE_FIND_ROOT_PATH $ENV{HOME}/raspberrypi/rootfs)" >> ~/home/raspberrypi/pi.cmake
+echo "SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)" >> ~/home/raspberrypi/pi.cmake
+echo "SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)" >> ~/home/raspberrypi/pi.cmake
+echo "SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)" >> ~/home/raspberrypi/pi.cmake
 
 git clone https://github.com/raspberrypi/tools.git ~/raspberrypi/tools
-cd ~
 echo export PATH=$PATH:$HOME/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin >> ~/.bashrc
 echo export RASPBIAN_ROOTFS=~/raspberrypi/rootfs >> ~/.bashrc
 . ~/.bashrc
--- .travis/script.sh
+++ .travis/script.sh
@@ -3,8 +3,6 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 set -x # Print commands and their arguments as they are executed.
 
-exit 0
-
 cd ~/motom001/linphone4raspberry/linphone-cmake-builder
 python prepare.py python-raspberry
 make -C WORK/cmake-python-raspberry
